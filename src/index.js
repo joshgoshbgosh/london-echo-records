302972b16c42c91184992dfa78c2c120b1527826
joshgoshbgosh <69131589+joshgoshbgosh@users.noreply.github.com>
2021-08-04 13:54:24 -0400

    router installed

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router,Switch,Route,  Link} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Router basename="london-echo-records/">
     <App />
   </React.StrictMode>,
   document.getElementById('root')
