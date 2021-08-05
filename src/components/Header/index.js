e0fb5c127ffac62631a82597dd262dff88a1e63f
joshgoshbgosh <69131589+joshgoshbgosh@users.noreply.github.com>
2021-08-05 17:18:46 -0400

    nav update

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink} from 'react-router-dom';
-
+import headermic from './../../images/headermic.png';
 import './index.css';
 
 function Header(props){
@@ -9,10 +9,18 @@ function Header(props){
       <div className="social-media">
         <a className="social" href="#"><i class="fab fa-facebook-f"></i></a>
         <a className="social" href="#"><i class="fab fa-instagram"></i></a>
-        <a className="social" href="#"><i class="fab fa-youtube"></i></a>
         <a className="social" href="#"><i class="fab fa-twitter"></i></a>
         <a className="social" href="#"><i class="fab fa-vimeo-v"></i></a>
       </div>
+      <nav className="menu">
+
+        <ul className="list-wrap">
+          <li><a className="list"href="#">HOME</a></li>
+          <li><a className="list"href="#">ABOUT</a></li>
+          <li><a className="list"href="#">GEAR</a></li>
+          <li><a className="list"href="#">CONTACT</a></li>
+        </ul>
+      </nav>
     </header>
   );
 }
